c652754ff46bbfc300b92a84e5642a48e646113e
Davide Dongiovanni <dongiovannidavide@gmail.com>
2018-06-19 16:44:40 +0200

    Add jfx interface client-side

--- src/client/Kmeans.java
+++ src/client/Kmeans.java
@@ -1,34 +1,88 @@
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Kmeans extends Application{
-	private Tab dbTab,fileTab;
+public class Kmeans extends Application {
+	private OutputTab dbTab, fileTab;
 
 	public void start(Stage primaryStage) {
 		TabPane tabPane = new TabPane();
-		dbTab = new Tab();
-		fileTab = new Tab();
+		TextField tabName = new TextField();
+		TextField nCluster = new TextField();
+		TextField fileName = new TextField();
+		FlowPane dbTabUp = new FlowPane();
+		dbTabUp.getChildren().add(tabName);
+		dbTabUp.getChildren().add(nCluster);
+		dbTabUp.getChildren().add(fileName);
+		dbTab = new OutputTab(dbTabUp, "MINE", (e) -> {
+			try {
+				URL url = new URL("http://172.26.243.58:8080/MAP-Servlet/Servlet?command=DB&tabName="
+						+ tabName.getText() + "&nCluster=" + nCluster.getText());
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				ObjectInputStream in = new ObjectInputStream(conn.getInputStream());
+				String result = (String) in.readObject();
+				if (result.startsWith("Errore")) {
+					new TextInputDialog(result).showAndWait();
+				} else {
+					dbTab.clusterOutput.setText(result);
+				}
+			} catch (IOException | ClassNotFoundException e1) {
+				e1.printStackTrace();
+			}
+
+		});
+		dbTab.setText("DB");
+		// FlowPane fileTabUp = new FlowPane();
+		// fileTabUp.getChildren().add();
 		tabPane.getTabs().add(dbTab);
-		tabPane.getTabs().add(fileTab);
-		GridPane dbRoot = new GridPane();
-		FlowPane dbUp = new FlowPane();
-		
-		
-		
+		// tabPane.getTabs().add(fileTab);
+
+
+		Scene s = new Scene(tabPane, 300, 300);
+		primaryStage.setScene(s);
 		primaryStage.setTitle("CLIENT");
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch();
 	}
-	
-	//private class 
+
+	private class OutputTab extends Tab {
+		private GridPane root = new GridPane();
+		private Button executeButton = new Button();
+		private TextArea clusterOutput = new TextArea();
+
+		OutputTab(Node n, String button, EventHandler<ActionEvent> event) {
+			clusterOutput.setEditable(false);
+			ScrollPane outputPane = new ScrollPane(clusterOutput);
+			executeButton.setText(button);
+			executeButton.setOnAction(event);
+			root.add(n, 0, 0);
+			root.add(outputPane, 0, 1);
+			root.add(executeButton, 0, 2);
+			this.setContent(root);
+		}
+
+	}
 
 }
