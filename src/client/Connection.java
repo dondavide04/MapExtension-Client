59f19a9173db6ad3a04e071453adab255ce5cab7
Davide Dongiovanni <dongiovannidavide@gmail.com>
2018-06-25 09:47:18 +0200

    Update connection to remote

--- src/client/Connection.java
+++ src/client/Connection.java
@@ -14,40 +14,22 @@ import java.net.URL;
  *
  */
 class Connection {
-	final private String unibaIp = "172.26.243.58";
-	final private String hotSpotIp = "192.168.43.254";
-	final private String homeIp = "";
-	final private String port = "8080";
-	final private String servletUrl = "MAPE%20-%20Servlet/Servlet";
+	final private String remoteUrl = "http://mape-env.35smckpbkd.us-east-2.elasticbeanstalk.com/";
+	final private String servletUrl = "Servlet";
+	final private int connectTimeout = 2000;
 
 	ObjectInputStream getConnectionStream(String parameters) throws ServerConnectionFailedException {
 		ObjectInputStream in;
 		URL url;
 		HttpURLConnection conn;
 		try {
-			url = new URL("http://" + unibaIp + ":" + port + "/" + servletUrl + parameters);
+			url = new URL(remoteUrl + servletUrl + parameters);
 			conn = (HttpURLConnection) url.openConnection();
-			conn.setConnectTimeout(2000);
+			conn.setConnectTimeout(connectTimeout);
 			conn.connect();
 			in = new ObjectInputStream(conn.getInputStream());
 		} catch (IOException e) {
-			try {
-				url = new URL("http://" + hotSpotIp + ":" + port + "/" + servletUrl + parameters);
-				conn = (HttpURLConnection) url.openConnection();
-				conn.setConnectTimeout(2000);
-				conn.connect();
-				in = new ObjectInputStream(conn.getInputStream());
-			} catch (IOException e1) {
-				try {
-					url = new URL("http://" + homeIp + ":" + port + "/" + servletUrl + parameters);
-					conn = (HttpURLConnection) url.openConnection();
-					conn.setConnectTimeout(2000);
-					conn.connect();
-					in = new ObjectInputStream(conn.getInputStream());
-				} catch (IOException e2) {
-					throw new ServerConnectionFailedException();
-				}
-			}
+			throw new ServerConnectionFailedException();
 		}
 		return in;
 	}
